8878654b61133648eec9409372d259e5d2f2f58b
Valerii_Panov <val250640@mail.ru>
2024-09-02 08:45:11 +1000

    sem03

---
+++ Seminar01/tests/SaveEventCommandTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Application;
+use App\Commands\SaveEventCommand;
+use PHPUnit\Framework\TestCase;
+
+class SaveEventCommandTest extends TestCase
+{
+    /**
+     * @dataProvider getDataProvider
+     */
+    public function testIsNeedHelp(array $options, bool $isNeededResult): void
+    {
+        $saveEventCommand = new SaveEventCommand(new Application(dirname(__DIR__)));
+
+        $result = $saveEventCommand->isNeedHelp($options);
+
+        self::assertEquals($result, $isNeededResult);
+    }
+
+    public static function getDataProvider(): array
+    {
+        return [
+          [
+              [
+                'name' => 'some data',
+                  'text' => 'some data',
+                  'receiver' => 'some data',
+                  'cron' => 'some data'
+              ],
+              false
+          ],
+            [
+                [
+                    'name' => 'some data',
+                    'text' => 'some data',
+                    'receiver' => 'some data',
+                    'cron' => 'some data',
+                    'help' => 'some data',
+                    'h' => 'some data'
+                ],
+                true
+            ]
+        ];
+    }
+}
